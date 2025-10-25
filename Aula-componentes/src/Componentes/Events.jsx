14313bb847b37370849d3a4c09eda6d415682c6a
Antonio Rivaldo21 <rival2silva365@gmail.com>
2025-10-24 21:50:29 -0300

    Projeto
    ok.

--- Aula-componentes/src/Componentes/Events.jsx
+++ Aula-componentes/src/Componentes/Events.jsx
@@ -2,6 +2,13 @@ const Events = () => {
     const hundleClick = () => {
         console.log("Executou Clique")
     }
+    const renderSomething = (x) =>{
+        if(x){
+            return <h1>Processo em Renderizado!</h1>
+        }else{
+            return <h1>Processo Renderizado!</h1>
+        }
+    }
     return (
         <div>
             <div>
@@ -12,7 +19,11 @@ const Events = () => {
 
         <div>
             <button onClick={hundleClick}> Novo Clique</button>
+
          </div>
+<div>
+    {renderSomething(true)}
+</div>
         </div>
     );
 };
